3e9b52726abb8f19e83d753a018de5117d68eb66
Michal Zelinka <misacek@misacek.net>
2022-07-06 04:57:21 +0200

    Basic observing experiment, Dark Mode, logo injection, responder actions

--- TweetDuck/AppDelegate.swift
+++ TweetDuck/AppDelegate.swift
@@ -14,12 +14,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet var window: NSWindow!
     @IBOutlet var webView: WKWebView!
 
+    var appearanceObservation: NSKeyValueObservation?
+    var progressObservation: NSKeyValueObservation?
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         webView.navigationDelegate = self
         webView.uiDelegate = self
 
         webView.configuration.preferences.setValue(true, forKey: "developerExtrasEnabled")
+
+        appearanceObservation = webView.observe(\.effectiveAppearance) { _,_ in self.checkCurrentAppearance() }
+        progressObservation = webView.observe(\.estimatedProgress) { _,_ in self.onProgressUpdate() }
 
         webView.load(URLRequest(url: URL(string: "https://tweetdeck.twitter.com")!))
     }
@@ -51,12 +57,85 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 extension AppDelegate {
 
+    func showSettings() {
+        let script = "new TD.components.GlobalSettings"
+        webView.evaluateJavaScript(script)
+    }
+
+    func clickFindButton() {
+        let script = "document.querySelector('button.app-search-fake').click()"
+        webView.evaluateJavaScript(script)
+    }
+
+    func checkCurrentAppearance() {
+
+        let script = """
+            (function loop_currentAppearance(){
+                setTimeout(function() {
+                    if (TD && TD.ready) {
+                        var mode = TD.settings.getTheme()
+                        var desiredMode = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
+                        if (mode != desiredMode) { TD.settings.setTheme(desiredMode) }
+                    } else loop_currentAppearance()
+              }, 500)
+            })()
+            """
+
+        webView.evaluateJavaScript(script)
+    }
+
+    func injectLogo() {
+
+        var imageRect = NSRect(origin: .zero, size: .init(width: 36, height: 36))
+        let image = NSApp.applicationIconImage!
+        let imageRep = NSBitmapImageRep(cgImage: image.cgImage(forProposedRect: &imageRect, context: nil, hints: nil)!)
+        imageRep.size = imageRect.size
+
+        let imageData = imageRep.representation(using: .png, properties: [:])!
+
+        let base64String = imageData.base64EncodedString()
+        let htmlString = "data:image/png;base64,\(base64String)"
+
+        let script = """
+            (function loop_injectLogo(){
+                setTimeout(function() {
+                    var logoElm = document.getElementsByClassName('tweetdeck-logo')[0]
+                    if (logoElm != null) {
+                        logoElm.style = "background: none"
+                        logoElm.classList.toggle('width--26', false)
+                        logoElm.classList.toggle('height--24', false)
+                        logoElm.innerHTML = "<img src='\(htmlString)' style='width: 36px; height: 36px'>"
+                    } else loop_injectLogo()
+              }, 500)
+            })()
+            """
+
+        webView.evaluateJavaScript(script)
+    }
+
+    func onProgressUpdate() {
+        if webView.url?.host == "tweetdeck.twitter.com" && webView.estimatedProgress >= 1 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.checkCurrentAppearance()
+                self.injectLogo()
+            }
+        }
+    }
+
     @objc func dockOpen() {
         window.makeKeyAndOrderFront(nil)
     }
 
     @IBAction func showWindow(_ sender: Any?) {
         window.makeKeyAndOrderFront(sender)
+    }
+
+    @IBAction func performFindPanelAction(_ sender: Any?) {
+        clickFindButton()
+    }
+
+    @IBAction func showPreferences(_ sender: Any?) {
+        showSettings()
     }
 }
 
